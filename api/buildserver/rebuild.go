c4e19dd370ce30025df76e867104226f8abded85
Samuel Nelson <valczir.darkvein@gmail.com>
2017-05-25 00:47:16 -0600

    Make a Rebuild method on the buildserver.
    This will totally work, right?

---
+++ api/buildserver/rebuild.go
@@ -0,0 +1,45 @@
+package buildserver
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"code.cloudfoundry.org/lager"
+
+	"github.com/concourse/atc"
+	"github.com/concourse/atc/api/present"
+	"github.com/concourse/atc/dbng"
+)
+
+func (s *Server) Rebuild(build dbng.Build) http.Handler {
+	hLog := s.logger.Session("rebuild")
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var plan atc.Plan
+		err := json.NewDecoder(r.Body).Decode(&plan)
+		if err != nil {
+			hLog.Info("malformed-request", lager.Data{"error": err.Error()})
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		err := build.Reset()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		engineBuild, err := s.engine.CreateBuild(hLog, build, plan)
+		if err != nil {
+			hLog.Error("failed-to-start-build", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		engineBuild.Resume(hLog)
+
+		w.WriteHeader(http.StatusOK)
+
+		json.NewEncoder(w).Encode(present.Build(build))
+	})
+}
